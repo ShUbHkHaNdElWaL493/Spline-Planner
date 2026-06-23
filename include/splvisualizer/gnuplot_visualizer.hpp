c2a0150eef1490a6b1b5073b6e176352a3521139
Shubh Khandelwal <shubh4664@gmail.com>
2026-06-23 15:55:42 +0530

    Reverted gnuplot_visualizer to the old version

--- include/splvisualizer/gnuplot_visualizer.hpp
+++ include/splvisualizer/gnuplot_visualizer.hpp
@@ -3,74 +3,59 @@
 */
 
 #pragma once
-#include <stdexcept>
+#include <iostream>
 #include "visualizer.hpp"
 
 namespace splvisualizer
 {
-    class GnuplotVisualizer : public Visualizer
+    class GnuplotVisualizer : Visualizer
     {
 
         private:
 
-            FILE* pipe_multiplot;
-            FILE* pipe_visualizer;
+            FILE* gnuplot_pipe;
 
         public:
 
             GnuplotVisualizer(const std::pair<std::vector<double>, spl::Trajectory>& path) :
             Visualizer(path),
-            pipe_multiplot(popen("gnuplot", "w"))
+            gnuplot_pipe(popen("gnuplot", "w"))
             {
 
-                if (!pipe_multiplot)
+                if (!gnuplot_pipe)
                 {
-                    std::runtime_error("Failed to open pipe_multiplot.");
+                    std::cerr << "<ERROR>: Failed to open gnuplot pipe." << std::endl;
                 }
 
                 size_t num_dims = this->trajectory.pos[0].cols();
-
-                if (num_dims == 2 || num_dims == 3)
-                {
-                    pipe_visualizer = popen("gnuplot", "w");
-                    if (!pipe_visualizer)
-                    {
-                        std::runtime_error("Failed to open pipe_visualizer.");
-                    }
-                }
-
-                fprintf(pipe_multiplot, "set terminal qt size 800, 1200\n");
-                fprintf(pipe_multiplot, "set multiplot layout %zu,1 title 'gnuplot Visualizer' font ',12'\n", num_dims);
+                fprintf(gnuplot_pipe, "set terminal qt size 800, 1200\n");
+                fprintf(gnuplot_pipe, "set multiplot layout %zu,1 title 'gnuplot Visualizer' font ',12'\n", num_dims);
 
                 for (size_t i = 0; i < num_dims; ++i)
                 {
-                    fprintf(pipe_multiplot, "set title 'Dimension %zu vs Time'\n", i + 1);
-                    fprintf(pipe_multiplot, "set xlabel 'Time (s)'\n");
-                    fprintf(pipe_multiplot, "set ylabel 'Dimension %zu'\n", i + 1);
-                    fprintf(pipe_multiplot, "set xrange [0.0:20.0]\n");
-                    fprintf(pipe_multiplot, "set yrange [-2.0:2.0]\n");
-                    fprintf(pipe_multiplot, "plot '-' with lines lw 2 lc rgb 'red' title 'D%zu(t)'\n", i + 1);
+                    fprintf(gnuplot_pipe, "set title 'Dimension %zu vs Time'\n", i + 1);
+                    fprintf(gnuplot_pipe, "set xlabel 'Time (s)'\n");
+                    fprintf(gnuplot_pipe, "set ylabel 'Dimension %zu'\n", i + 1);
+                    fprintf(gnuplot_pipe, "set xrange [0.0:20.0]\n");
+                    fprintf(gnuplot_pipe, "set yrange [-2.0:2.0]\n");
+                    fprintf(gnuplot_pipe, "plot '-' with lines lw 2 lc rgb 'red' title 'D%zu(t)'\n", i + 1);
                     for (size_t j = 0; j < this->u.size(); ++j)
                     {
-                        fprintf(pipe_multiplot, "%f %f\n", this->u[j], this->trajectory.pos[j](i));
+                        fprintf(gnuplot_pipe, "%f %f\n", this->u[j], this->trajectory.pos[j](i));
                     }
-                    fprintf(pipe_multiplot, "e\n");
+                    fprintf(gnuplot_pipe, "e\n");
                 }
 
-                fprintf(pipe_multiplot, "unset multiplot\n");
-                fflush(pipe_multiplot);
+                fprintf(gnuplot_pipe, "unset multiplot\n");
+                fflush(gnuplot_pipe);
 
             }
 
             ~GnuplotVisualizer()
             {
-                if (pipe_multiplot)
+                if (gnuplot_pipe)
                 {
-                    pclose(pipe_multiplot);
-                }
-                if (pipe_visualizer)
-                {
-                    pclose(pipe_visualizer);
+                    pclose(gnuplot_pipe);
                 }
             }
 
@@ -78,77 +63,46 @@ namespace splvisualizer
             {
 
                 size_t num_dims = this->trajectory.pos[0].cols();
-                
-                // fprintf(pipe_multiplot, "set terminal qt size 800, 1200\n");
-                // fprintf(pipe_multiplot, "set multiplot layout %zu,1 title 'gnuplot Visualizer' font ',12'\n", num_dims);
+                fprintf(gnuplot_pipe, "set title 'gnuplot Visualizer' font ',12'\n");
 
-                // for (size_t i = 0; i < num_dims; ++i)
-                // {
-                //     fprintf(pipe_multiplot, "set title 'Dimension %zu vs Time'\n", i + 1);
-                //     fprintf(pipe_multiplot, "set xlabel 'Time (s)'\n");
-                //     fprintf(pipe_multiplot, "set ylabel 'Dimension %zu'\n", i + 1);
-                //     fprintf(pipe_multiplot, "set yrange [-2.0:2.0]\n");
-                //     fprintf(pipe_multiplot, "plot '-' with lines lw 2 lc rgb 'black' title 'Trajectory', \\\n");
-                //     fprintf(pipe_multiplot, "     '-' with lines lw 2 lc rgb 'red' title 'D%zu(t)'\n", i + 1);
-                //     for (size_t j = 0; j < this->u.size(); ++j)
-                //     {
-                //         fprintf(pipe_multiplot, "%f %f\n", this->u[j], q.back()(i));
-                //     }
-                //     fprintf(pipe_multiplot, "e\n");
-                //     for (size_t j = 0; j < this->u.size(); ++j)
-                //     {
-                //         fprintf(pipe_multiplot, "%f %f\n", this->u[j], this->trajectory.pos[j](i));
-                //     }
-                //     fprintf(pipe_multiplot, "e\n");
-                // }
+                fprintf(gnuplot_pipe, "set xlabel 'X'\n");
+                fprintf(gnuplot_pipe, "set ylabel 'Y'\n");
+                fprintf(gnuplot_pipe, "set xrange [-2.0:2.0]\n");
+                fprintf(gnuplot_pipe, "set yrange [-2.0:2.0]\n");
 
-                // fprintf(pipe_multiplot, "unset multiplot\n");
-                // fflush(pipe_multiplot);
-
-                if (pipe_visualizer)
+                if (num_dims == 2)
                 {
-
-                    fprintf(pipe_visualizer, "set terminal qt size 800, 800 title 'gnuplot Visualizer' font ',12'\n");
-                    fprintf(pipe_visualizer, "set xlabel 'X'\n");
-                    fprintf(pipe_visualizer, "set ylabel 'Y'\n");
-                    fprintf(pipe_visualizer, "set xrange [-2.0:2.0]\n");
-                    fprintf(pipe_visualizer, "set yrange [-2.0:2.0]\n");
-
-                    if (num_dims == 2)
+                    fprintf(gnuplot_pipe, "plot '-' with linespoints lw 4 pt 7 ps 1.5 lc rgb 'black' title 'Links', \\\n");
+                    fprintf(gnuplot_pipe, "     '-' with lines lw 2 lc rgb 'magenta' title 'Path'\n");
+                    for (const spl::VectorRepresentation& joint_position : q)
                     {
-                        fprintf(pipe_visualizer, "plot '-' with linespoints lw 4 pt 7 ps 1.5 lc rgb 'black' title 'Links', \\\n");
-                        fprintf(pipe_visualizer, "     '-' with lines lw 2 lc rgb 'red' title 'Path'\n");
-                        for (const spl::VectorRepresentation& joint_position : q)
-                        {
-                            fprintf(pipe_visualizer, "%f %f\n", joint_position(0), joint_position(1));
-                        }
-                        fprintf(pipe_visualizer, "e\n");
-                        for (const spl::VectorRepresentation& position_vector : this->trajectory.pos)
-                        {
-                            fprintf(pipe_visualizer, "%f %f\n", position_vector(0), position_vector(1));
-                        }
-                        fprintf(pipe_visualizer, "e\n");
-                    } else if (num_dims == 3)
-                    {
-                        fprintf(pipe_visualizer, "set zlabel 'Z'\n");
-                        fprintf(pipe_visualizer, "set zrange [-2.0:2.0]\n");
-                        fprintf(pipe_visualizer, "splot '-' with linespoints lw 4 pt 7 ps 1.5 lc rgb 'black' title 'Links', \\\n");
-                        fprintf(pipe_visualizer, "      '-' with lines lw 2 lc rgb 'red' title 'Path'\n");
-                        for (const spl::VectorRepresentation& joint_position : q)
-                        {
-                            fprintf(pipe_visualizer, "%f %f %f\n", joint_position(0), joint_position(1), joint_position(2));
-                        }
-                        fprintf(pipe_visualizer, "e\n");
-                        for (const spl::VectorRepresentation& position_vector : this->trajectory.pos)
-                        {
-                            fprintf(pipe_visualizer, "%f %f %f\n", position_vector(0), position_vector(1), position_vector(2));
-                        }
-                        fprintf(pipe_visualizer, "e\n");
+                        fprintf(gnuplot_pipe, "%f %f\n", joint_position(0), joint_position(1));
                     }
-
-                    fflush(pipe_visualizer);
-
+                    fprintf(gnuplot_pipe, "e\n");
+                    for (const spl::VectorRepresentation& position_vector : this->trajectory.pos)
+                    {
+                        fprintf(gnuplot_pipe, "%f %f\n", position_vector(0), position_vector(1));
+                    }
+                    fprintf(gnuplot_pipe, "e\n");
+                } else if (num_dims == 3)
+                {
+                    fprintf(gnuplot_pipe, "set zlabel 'Z'\n");
+                    fprintf(gnuplot_pipe, "set zrange [-2.0:2.0]\n");
+                    fprintf(gnuplot_pipe, "splot '-' with linespoints lw 4 pt 7 ps 1.5 lc rgb 'black' title 'Links', \\\n");
+                    fprintf(gnuplot_pipe, "      '-' with lines lw 2 lc rgb 'magenta' title 'Path'\n");
+                    for (const spl::VectorRepresentation& joint_position : q)
+                    {
+                        fprintf(gnuplot_pipe, "%f %f %f\n", joint_position(0), joint_position(1), joint_position(2));
+                    }
+                    fprintf(gnuplot_pipe, "e\n");
+                    for (const spl::VectorRepresentation& position_vector : this->trajectory.pos)
+                    {
+                        fprintf(gnuplot_pipe, "%f %f %f\n", position_vector(0), position_vector(1), position_vector(2));
+                    }
+                    fprintf(gnuplot_pipe, "e\n");
                 }
+
+                fflush(gnuplot_pipe);
 
             }
 
